64a145534205db9b59bc7be138338601efd3d85f
liushihao <820016069@qq.com>
2022-05-30 16:54:39 +0800

    pref：优化了vite

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,70 @@
 <template>
-    <div class="app">
-        <!-- 头部栏   right-text="按钮"  -->
-            <van-nav-bar
-                :title="title"
-                left-text=""
-                left-arrow
-                v-show="isShnav"
-                @click-left="$router.back()"
-                />
+  <div class="app">
+    <!-- 头部栏   right-text="按钮"  -->
+    <van-nav-bar :title="title" left-text="" left-arrow v-show="isShnav" @click-left="$router.back()" />
 
-        
-        
-        
-        <router-view></router-view>
-        
-        
-    </div>
+    <router-view></router-view>
+
+  </div>
 </template>
 
 <script>
-    export default {
-        name:"app",
-        data(){
-            return {
-                title:'买亿点',
-                isShnav:false
-            }
-        },
-        watch:{
-            "$route":{
-                handler:function(newRoute){
-                    let {isShownav,title} = newRoute.meta
-                    this.title = title
-                    if(isShownav){
-                        this.isShnav = false
-                    }else{
-                        this.isShnav = true
-                    }
-                },
-                immediate:true
-            }
+export default {
+  name: "app",
+  data() {
+    return {
+      title: '买亿点',
+      isShnav: false,
+      network: window.navigator.onLine
+    }
+  },
+  watch: {
+    "$route": {
+      handler: function (newRoute) {
+        let { isShownav, title } = newRoute.meta
+        this.title = title
+        if (isShownav) {
+          this.isShnav = false
+        } else {
+          this.isShnav = true
+        }
+      },
+      immediate: true
+    },
+    network(){
+        if(this.network === true){
+            this.$toast('网络连接成功')
+        }else{
+            this.$toast('网络断开，请检查网络')
         }
     }
+  },
+  methods: {
+      //判断是否有网络
+    onNetwork(e) {
+      this.network = e.type === "online" ? true : false
+    }
+  },
+  mounted() {
+    window.addEventListener('online', this.onNetwork)
+    window.addEventListener('offline', this.onNetwork)
+  }
+}
 </script>
 <style lang="scss">
 //导入scss公共样式
-@import './assets/scss/Public.scss';
-    html{
-        scroll-behavior: smooth;    //滚动动画变的丝滑
-    }
-    *{
-        box-sizing: border-box;
-        font-size: 14px;
-    }
-    .app{
-        min-width: 320px;
-        max-width: 750px;
-        margin: auto;
-    }
+@import "./assets/scss/Public.scss";
+html {
+  scroll-behavior: smooth; //滚动动画变的丝滑
+}
+* {
+  box-sizing: border-box;
+  font-size: 14px;
+}
+.app {
+  min-width: 320px;
+  max-width: 750px;
+  margin: auto;
+}
 </style>
 
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,49 @@
-import { createVuePlugin } from 'vite-plugin-vue2'
+import { createVuePlugin } from 'vite-plugin-vue2';
+import commonjs from "rollup-plugin-commonjs";
+import externalGlobals from 'rollup-plugin-external-globals';
 
 export default {
-  plugins: [createVuePlugin()]
+  plugins: [createVuePlugin()],
+  //前端用于跨域
+  // server:{
+  //   proxy:{
+  //     '/myapi':{
+  //       target:'http://localhost:4000', //代理域名网址
+  //       changeOrigin:true,  //默认设置为true，保留请求头
+  //       rewrite:(path) => path.replace(/^\/myapi/,'') //重写url路径，设置为空
+  //     },
+  //   }
+  // },
+  resolve: {
+    // 设置路径别名
+    alias: {
+      '@': '/src',
+      '@api': '/src/api'
+    },
+    // 省略扩展名
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+  },
+  //生产环境去除console.log（）
+  build:{
+    minify: "esbuild",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      }
+    }
+  },
+  //把小于8k的图片转换成base64形式，减少服务器解码压力，默认是4kb，*2则8kb
+  assetsInlineLimit: 4096 * 2,
+  //配置cdn加速，但是cdn必须要稳定的网站，不然会出现请求不到数据
+  rollupOptions: {
+    external: ["axios", "moment"],
+    plugins: [
+      commonjs(),
+      externalGlobals({
+        axios: "axios",
+        moment: "moment",
+      }),
+    ],
+  },
 }
